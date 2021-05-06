d7f717e0c9ad829ec9acfd6f68266dd7ee303f05
Nextiva challenge resolver <cristinagrl@hotmail.com>
2021-05-06 09:33:28 -0500

    autenticacion de correo

---
+++ src/controler/auth.js
@@ -0,0 +1,13 @@
+export const createAccount = (email, password) => firebase.auth()
+  .createUserWithEmailAndPassword(email, password);
+/* firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+  */
--- src/view/signup.js
+++ src/view/signup.js
@@ -1,15 +1,24 @@
+import { createAccount } from '../controler/auth.js';
+
 export default () => {
   const viewHome = `
+  <form>
   <p><input type="button" id="return" onclick="location.href='#/login';" value="Return"></p>
-  <p><input type="text" id="FirstName" placeholder="First Name"></p>
-  <p><input type="text" id="LastName" placeholder="Last Name"></p>
-  <p><input type="email" id="Email" placeholder="Email"></p>
+  <p><input type="text" id="firstName" placeholder="First Name"></p>
+  <p><input type="text" id="lastName" placeholder="Last Name"></p>
+  <p><input type="email" id="email" placeholder="Email"></p>
   <p><input type="password" id="txtPass" placeholder="Password"></p>
-  <p><input type="button" id="SignUp" value="Sign Up"></p>
+  <p><button type="submit" id="signUp">Sign Up</button></p>
+  </form>
   `;
 
   const divElem = document.createElement('div');
   divElem.innerHTML = viewHome;
-
+  const datos = divElem.querySelector('#signUp');
+  datos.addEventListener('click', () => {
+    const email = divElem.querySelector('#email').value;
+    const password = divElem.querySelector('#txtPass').value;
+    createAccount(email, password).then((result) => console.log('Se registró exitosamente', result));
+  });
   return divElem;
 };
